2f49160a1679fd7d73473f0ac50487ef24d1d8fc
RoPallas <Rocio.orquestas@gmail.com>
2023-07-06 08:19:39 -0700

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,25 @@ from datetime import datetime
 class BaseModel():
     """Class BaseModel"""
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """"""
+        if kwargs:
+            for k, v in kwargs.items():
+                if k in ['created_at', 'updated_at']:
+                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif k != '__class__':
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
+        """"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """"""
         new_dict = self.__dict__
         new_dict['__class__'] = self.__class__.__name__
         new_dict['updated_at'] = self.updated_at.isoformat()
@@ -23,4 +33,5 @@ class BaseModel():
         return new_dict
 
     def __str__(self):
+        """"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
