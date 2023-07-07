f6e0143b63fee79b78c41a766a3cfe6b6cf57b1d
RoPallas <Rocio.orquestas@gmail.com>
2023-07-07 07:10:23 -0700

    Fix init problems in BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """Class BaseModel"""
 import uuid
 from datetime import datetime
-from models import storage
+import models
 
 
 class BaseModel():
@@ -11,23 +11,26 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         """"""
         if kwargs:
+            format_date = '%Y-%m-%dT%H:%M:%S.%f'
             for k, v in kwargs.items():
-                if k in ['created_at', 'updated_at']:
-                    setattr(self, k, datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
-                elif k == '__class__':
-                    continue
+                if k == '__class__':
+                    pass
+                elif k == 'created_at':
+                    self.created_at = datetime.strptime(v, format_date)
+                elif k == 'updated_at':
+                    self.updated_at = datetime.strptime(v, format_date)
                 else:
                     setattr(self, k, v)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
+            models.storage.new(self)
 
     def save(self):
         """"""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """"""
