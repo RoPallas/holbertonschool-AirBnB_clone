8d0f3a0cae019aaae452f543f2f12e7499f6b5be
RoPallas <Rocio.orquestas@gmail.com>
2023-07-07 08:01:21 -0700

    Fix undocumented commands

--- console.py
+++ console.py
@@ -9,15 +9,15 @@ class HBNBCommand(cmd.Cmd):
     prompt = '(hbtn) '
 
     def do_quit(self, arg):
-        """"""
+        """HOla"""
         return True
 
     def do_EOF(self, arg):
-        """"""
+        """Hola"""
         return True
 
     def emptyline(self):
-        """"""
+        """Hola"""
         pass
 
 if __name__ == '__main__':
