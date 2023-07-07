b154dee8d227ced34f88f5a9e855fe19ea807519
RoPallas <Rocio.orquestas@gmail.com>
2023-07-07 08:05:27 -0700

    Fix undocumented commands

--- console.py
+++ console.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
-""""""
+"""Console for AirBnB project"""
 import cmd
 
 
 class HBNBCommand(cmd.Cmd):
-    """"""
+    """Console"""
 
     prompt = '(hbtn) '
 
     def do_quit(self, arg):
-        """HOla"""
+        """Exit to the console"""
         return True
 
     def do_EOF(self, arg):
-        """Hola"""
+        """Exit to the console"""
         return True
 
     def emptyline(self):
-        """Hola"""
+        """Pass an empty line"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
