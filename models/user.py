7b7e2ddff0348e5d5f1773bf73614dd39f05370d
RoPallas <Rocio.orquestas@gmail.com>
2023-07-07 13:13:51 -0700

    Task 8, fix attr

--- models/user.py
+++ models/user.py
@@ -6,10 +6,7 @@ from models.base_model import BaseModel
 class User(BaseModel):
     """Class User"""
 
-    def __init__(self, *args, **kwargs):
-        """Instance an User"""
-        super().__init__(self, *args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
